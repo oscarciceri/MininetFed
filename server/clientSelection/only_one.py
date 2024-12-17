1f9b2d87fd46b4b4e2befda1402ad01cfdd7c4cf
jjakob10 <johann.bastos@edu.ufes.br>
2024-12-17 17:08:24 -0300

    feat: only_one client selector

---
+++ server/clientSelection/only_one.py
@@ -0,0 +1,10 @@
+
+# OnlyOne: Seleciona apenas um cliente
+class OnlyOne:
+    def __init__(self):
+        pass
+
+    def select_trainers_for_round(self, trainer_list, metrics):
+        s = [trainer_list[0]]
+
+        return s
