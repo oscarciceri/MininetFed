8c44e879efadac246ba8d34a44e2d5956424fe22
jjakob10 <johann.bastos@edu.ufes.br>
2024-12-17 17:37:28 -0300

    fix: create docker

--- docker/create_images.sh
+++ docker/create_images.sh
@@ -7,8 +7,10 @@ sudo docker buildx create --driver-opt image=moby/buildkit:master  \
 sudo docker buildx use insecure-builder
 
 
-sudo docker buildx build -o type=docker --allow security.insecure --tag "mininetfed:broker" -f docker/Dockerfile.broker . 
-sudo docker buildx build -o type=docker --allow security.insecure --tag "mininetfed:container" -f docker/Dockerfile.container .
-sudo docker buildx build -o type=docker --allow security.insecure  --tag "mininetfed:client" -f docker/Dockerfile.client .
-sudo docker buildx build -o type=docker --allow security.insecure --tag "mininetfed:server" -f docker/Dockerfile.server .
+# sudo docker buildx build -o type=docker --allow security.insecure --tag "mininetfed:broker" -f docker/Dockerfile.broker . 
+# sudo docker buildx build -o type=docker --allow security.insecure --tag "mininetfed:container" -f docker/Dockerfile.container .
+# sudo docker buildx build -o type=docker --allow security.insecure  --tag "mininetfed:client" -f docker/Dockerfile.client .
+# sudo docker buildx build -o type=docker --allow security.insecure --tag "mininetfed:server" -f docker/Dockerfile.server .
+# sudo docker buildx build -o type=docker --allow security.insecure --tag "mininetfed:serversensor" -f docker/Dockerfile.serversensor .
+sudo docker buildx build -o type=docker --allow security.insecure --tag "mininetfed:clientsensor" -f docker/Dockerfile.clientsensor .
 sudo docker buildx rm insecure-builder
