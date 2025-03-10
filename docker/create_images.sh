7e03c2ca7dd4d7a868c76a3a86cb8fdb2f989025
jjakob10 <johann.bastos@edu.ufes.br>
2025-03-09 22:36:56 -0300

    feat: autowait image

--- docker/create_images.sh
+++ docker/create_images.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo docker build -t "mininetfed:broker" -f docker/Dockerfile.broker .
+sudo docker build -t "mininetfed:auto_wait" -f docker/Dockerfile.auto_wait .
 sudo docker build -t "mininetfed:client" -f docker/Dockerfile.client .
 sudo docker build -t "mininetfed:server" -f docker/Dockerfile.server .
 sudo docker build -t "mininetfed:serversensor" -f docker/Dockerfile.serversensor .
